fb27f729a52d231569d2398f8d542ed22e804eaf
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-10-22 15:12:35 +0200

    Added @vtfk/logger

--- GetDocuments/index.js
+++ GetDocuments/index.js
@@ -1,15 +1,19 @@
+const { logConfig, logger } = require('@vtfk/logger')
+const { DEMO } = require('../config')
 const { documentsRequest } = require('../lib/generate-request')
 const getDocuments = require('../lib/isi-lokal')
 const { getVariables, updateVariables } = require('../lib/handle-variables')
 
 module.exports = async function (context, req) {
+  logConfig({
+    prefix: DEMO ? 'DEMO' : ''
+  })
+
   try {
+    logger('info', ['GetDocuments', 'start'])
     const { uuid, counter, docCount, county } = await getVariables()
     const request = documentsRequest(uuid, counter, docCount, county)
-    const response = await getDocuments({
-      type: 'GET',
-      request
-    })
+    const response = await getDocuments('GET', request)
 
     // increase counter with response.length and 1 for the fetch request itself
     await updateVariables({
@@ -20,12 +24,13 @@ module.exports = async function (context, req) {
 
     // hent annen drit fra database og fyll ut på hvert dokument (fortsatt aktuelt?)
 
+    logger('info', ['GetDocuments', 'finish'])
     return {
       status: 200,
       body: response
     }
   } catch (error) {
-    console.log(error)
+    logger('error', ['GetDocuments', 'error', error])
     return {
       status: 500,
       body: error
--- Status/index.js
+++ Status/index.js
@@ -1,3 +1,5 @@
+const { logConfig, logger } = require('@vtfk/logger')
+const { DEMO } = require('../config')
 const { statusRequest } = require('../lib/generate-request')
 const setStatus = require('../lib/isi-lokal')
 const { getVariables, updateVariables } = require('../lib/handle-variables')
@@ -6,14 +8,16 @@ const hasData = data => Object.getOwnPropertyNames(data).length > 0
 const hasException = data => hasData(data) && data.Feiltype === 'EXCEPTION I STATUSOPPDATERING'
 
 module.exports = async function (context, req) {
+  logConfig({
+    prefix: DEMO ? 'DEMO' : ''
+  })
+
   try {
+    logger('info', ['Status', 'start'])
     const { docId, ssn } = req.body
     const { uuid, counter } = await getVariables()
     const request = statusRequest(uuid, counter, docId, ssn)
-    const response = await setStatus({
-      type: 'SET',
-      request
-    })
+    const response = await setStatus('SET', request)
 
     // increase counter with 1 for the fetch request itself
     await updateVariables({
@@ -21,13 +25,15 @@ module.exports = async function (context, req) {
       counter,
       increment: 1
     })
-
+    
+    const status = hasData(response) ? hasException(response) ? 218 : 500 : 200 // TODO: Figure out which 4xx code Logic App retry policy doesn't act on
+    logger('info', ['Status', status, 'finish'])
     return {
-      status: hasData(response) ? hasException(response) ? 218 : 500 : 200, // TODO: Figure out which 4xx code Logic App retry policy doesn't act on
+      status,
       body: response
     }
   } catch (error) {
-    console.log(error)
+    logger('error', ['Status', 500, 'error', error])
     return {
       status: 500,
       body: error
