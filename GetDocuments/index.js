c189eb487870736d47501b4f3601d01c20af07d3
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-10-22 14:19:58 +0200

    Added updating of variables

--- GetDocuments/index.js
+++ GetDocuments/index.js
@@ -1,17 +1,24 @@
 const { documentsRequest } = require('../lib/generate-request')
 const getDocuments = require('../lib/isi-lokal')
+const { getVariables, updateVariables } = require('../lib/handle-variables')
 
 module.exports = async function (context, req) {
   try {
-    const request = await documentsRequest()
+    const { uuid, counter, docCount, county } = await getVariables()
+    const request = documentsRequest(uuid, counter, docCount, county)
     const response = await getDocuments({
       type: 'GET',
       request
     })
 
-    // øk counter med response.length (overskriver den 9999, resett til 0 eller riktig løpenummer)
+    // increase counter with response.length and 1 for the fetch request itself
+    await updateVariables({
+      uuid,
+      counter,
+      increment: response.length + 1
+    })
 
-    // hent annen drit fra database og fyll ut på hvert dokument
+    // hent annen drit fra database og fyll ut på hvert dokument (fortsatt aktuelt?)
 
     return {
       status: 200,
--- Status/index.js
+++ Status/index.js
@@ -1,5 +1,6 @@
 const { statusRequest } = require('../lib/generate-request')
 const setStatus = require('../lib/isi-lokal')
+const { getVariables, updateVariables } = require('../lib/handle-variables')
 
 const hasData = data => Object.getOwnPropertyNames(data).length > 0
 const hasException = data => hasData(data) && data.Feiltype === 'EXCEPTION I STATUSOPPDATERING'
@@ -7,15 +8,19 @@ const hasException = data => hasData(data) && data.Feiltype === 'EXCEPTION I STA
 module.exports = async function (context, req) {
   try {
     const { docId, ssn } = req.body
-    const request = await statusRequest(docId, ssn)
+    const { uuid, counter } = await getVariables()
+    const request = statusRequest(uuid, counter, docId, ssn)
     const response = await setStatus({
       type: 'SET',
       request
     })
 
-    // øk counter med elements.length (overskriver den 9999, resett til 0 eller riktig løpenummer)
-
-    // hent annen drit fra database og fyll ut på hvert dokument
+    // increase counter with 1 for the fetch request itself
+    await updateVariables({
+      uuid,
+      counter,
+      increment: 1
+    })
 
     return {
       status: hasData(response) ? hasException(response) ? 218 : 500 : 200, // TODO: Figure out which 4xx code Logic App retry policy doesn't act on
