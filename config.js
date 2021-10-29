556fa9e86e268f69a8107a005ffafd26d3d55932
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-10-29 10:21:55 +0200

    Fetch variables from database

--- config.js
+++ config.js
@@ -6,7 +6,14 @@ module.exports = {
     GET_DOCUMENTS: process.env.ISI_GET_DOCUMENTS || 'HentDataForArkivering',
     SET_STATUS: process.env.SET_STATUS || 'LagreStatusArkiverteData'
   },
+  DB: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
+    table: process.env.DB_TABLE
+  },
   DEMO: (process.env.DEMO && process.env.DEMO.toLowerCase() === 'true') || false,
-  DEMO_DOC_COUNT: process.env.DEMO_DOC_COUNT,
-  DEMO_COUNTY: process.env.DEMO_COUNTY
+  DOC_COUNT: process.env.DOC_COUNT,
+  COUNTY: process.env.COUNTY
 }
